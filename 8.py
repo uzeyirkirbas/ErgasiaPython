9e0ad24014df3485f842114be9dbf054610f27a1
Üzeyir <uzeyirkirbas14@gmail.com>
2021-02-24 06:59:12 +0200

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,9 +1,7 @@
-import datetime
 import requests
-
 import urllib.request
 import json
-from time import sleep
+
 
 print("Please provide the name of the file without the '.txt' extension.")
 print(" ")
@@ -66,18 +64,3 @@ print("You have:")
 
 for sonunda in range(len(crypto_names_list)):
 	print(crypto_amount[sonunda] + " " + crypto_names_list[sonunda] + " which equals to " + Euro_List[sonunda] + " €")
-
-
-#def get_coin_data(coin):
-#    url="https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + crypto_names_for_url + "&tsyms=EUR&e=CCCAGG"
-#    r=urllib.request.urlopen(url)
-#    html=r.read()
-#    html=html.decode()
-#    d=json.loads(html)
-#    return d[coin]["EUR"]
-#
-# 
-#
-#while True:
-#    print(get_coin_data("ETH"))
-#    sleep(5)
